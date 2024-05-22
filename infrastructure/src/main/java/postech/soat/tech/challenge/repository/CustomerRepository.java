72baeb6605e0b8de33da47413ee6d3d0c5fd3a0f
Gabriel Paiva <gabriel.paiva@kenect.com>
2024-05-22 10:11:03 -0300

    test: Set up integration tests with TestContainers and RestAssured

--- infrastructure/src/main/java/postech/soat/tech/challenge/repository/CustomerRepository.java
+++ infrastructure/src/main/java/postech/soat/tech/challenge/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package postech.soat.tech.challenge.repository;
 import org.springframework.data.repository.CrudRepository;
 import postech.soat.tech.challenge.entity.Customer;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long>{
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
 }
---
+++ infrastructure/src/test/java/postech/soat/tech/challenge/config/DefaultResources.java
@@ -0,0 +1,54 @@
+package postech.soat.tech.challenge.config;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DefaultResources {
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:14.2"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUpRestAssured() {
+        RestAssured.baseURI = "http://localhost:" + port;
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+
+        String jdbcUrl = postgres.getJdbcUrl();
+        String username = postgres.getUsername();
+        String password = postgres.getPassword();
+
+        registry.add("spring.datasource.url", () -> jdbcUrl);
+        registry.add("spring.datasource.username", () -> username);
+        registry.add("spring.datasource.password", () -> password);
+        registry.add("spring.flyway.url", () -> jdbcUrl);
+        registry.add("spring.flyway.user", () -> username);
+        registry.add("spring.flyway.password", () -> password);
+
+    }
+}
