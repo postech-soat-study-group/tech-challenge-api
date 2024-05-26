48730eecf4e25cbd229958378e1df568693ab2e2
Felipe Tomm <felipe.tomm@gmail.com>
2024-05-26 15:53:08 -0300

    description: - Add get findByCpf in customer controller.

--- infrastructure/src/main/java/postech/soat/tech/challenge/api/controller/CustomerController.java
+++ infrastructure/src/main/java/postech/soat/tech/challenge/api/controller/CustomerController.java
@@ -2,15 +2,14 @@ package postech.soat.tech.challenge.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import postech.soat.tech.challenge.api.request.CustomerDTO;
 import postech.soat.tech.challenge.api.response.ApiResponse;
 import postech.soat.tech.challenge.model.Customer;
 import postech.soat.tech.challenge.port.input.CreateCustomerUseCase;
+import postech.soat.tech.challenge.port.input.FindCustomerUseCase;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -18,11 +17,19 @@ import postech.soat.tech.challenge.port.input.CreateCustomerUseCase;
 public class CustomerController {
 
     private final CreateCustomerUseCase createCustomerUseCase;
+    private final FindCustomerUseCase findCustomerUseCase;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO) {
         Customer createdCustomer = createCustomerUseCase.createCustomer(customerDTO.toCustomer());
         return new ApiResponse<>(CustomerDTO.toCustomerDTO(createdCustomer));
+    }
+
+    @GetMapping("findByCpf/{cpf}")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<Optional<Customer>> getCustomerByCpf(@PathVariable String cpf) {
+        Optional<Customer> customer = findCustomerUseCase.findByCpf(cpf);
+        return new ApiResponse<Optional<Customer>>(customer);
     }
 }
--- infrastructure/src/main/java/postech/soat/tech/challenge/config/UseCaseConfig.java
+++ infrastructure/src/main/java/postech/soat/tech/challenge/config/UseCaseConfig.java
@@ -7,12 +7,8 @@ import postech.soat.tech.challenge.persistence.repository.JpaCustomerRepository;
 import postech.soat.tech.challenge.persistence.repository.JpaCustomerRepositoryAdapter;
 import postech.soat.tech.challenge.persistence.repository.JpaProductRepository;
 import postech.soat.tech.challenge.persistence.repository.JpaProductRepositoryAdapter;
-import postech.soat.tech.challenge.port.input.CreateCustomerUseCase;
-import postech.soat.tech.challenge.port.input.CreateProductUseCase;
-import postech.soat.tech.challenge.port.input.DeleteProductUseCase;
-import postech.soat.tech.challenge.port.input.FindProductUseCase;
+import postech.soat.tech.challenge.port.input.*;
 import postech.soat.tech.challenge.port.output.CustomerRepository;
-import postech.soat.tech.challenge.port.input.UpdateProductUseCase;
 import postech.soat.tech.challenge.port.output.ProductRepository;
 
 @Configuration
@@ -55,4 +51,7 @@ public class UseCaseConfig {
     public CreateCustomerUseCase createCustomerUseCase() {
         return new CreateCustomerUseCase(customerRepository);
     }
+
+    @Bean
+    public FindCustomerUseCase findCustomerUseCase() { return new FindCustomerUseCase(customerRepository); }
 }
