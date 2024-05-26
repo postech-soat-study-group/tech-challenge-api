6c87fa297556d3367812cceb08d09a741951f681
Felipe Tomm <felipe.tomm@gmail.com>
2024-05-26 15:52:09 -0300

    description: - Add findCustomerUseCase.

---
+++ application/src/main/java/postech/soat/tech/challenge/port/input/FindCustomerUseCase.java
@@ -0,0 +1,15 @@
+package postech.soat.tech.challenge.port.input;
+
+import postech.soat.tech.challenge.model.Customer;
+import postech.soat.tech.challenge.port.output.CustomerRepository;
+
+import java.util.Optional;
+import java.util.logging.Logger;
+
+public class FindCustomerUseCase {
+    private final CustomerRepository customerRepository;
+
+    public FindCustomerUseCase(CustomerRepository customerRepository) { this.customerRepository = customerRepository; }
+
+    public Optional<Customer> findByCpf(String cpf) { return customerRepository.findByCpf(cpf); }
+}
