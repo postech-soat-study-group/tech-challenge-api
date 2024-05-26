bb9963334a3aed1556e7fb70215999a209eb22be
r4ulmanzas <raulmanzas@gmail.com>
2024-05-26 20:07:24 -0300

    refac: removes InvalidModelException since it it no longer used

--- domain/src/main/java/postech/soat/tech/challenge/model/InvalidModelException.java
+++
@@ -1,7 +0,0 @@
-package postech.soat.tech.challenge.model;
-
-public class InvalidModelException extends RuntimeException {
-    public InvalidModelException(String className, String message) {
-        super("Invalid property on %s: %s".formatted(className, message));
-    }
-}
